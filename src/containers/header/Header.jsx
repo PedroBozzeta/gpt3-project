1cfac150d9670af4588cf038c4dffc71036cf513
PedroBozzeta <108239149+PedroBozzeta@users.noreply.github.com>
2023-08-14 21:09:09 -0500

    Creacción del nav y header

---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
+const Header = () => {
+  return (
+    <div className="gpt3__header section_padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Lets Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
+          asperiores aliquam libero odio recusandae. Modi reiciendis cum eum.
+          Quaerat iusto aut corporis fugiat natus rem voluptatum eius incidunt
+          reiciendis quia!
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+        </div>
+        <p>1,600 people requested access to this platform </p>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
